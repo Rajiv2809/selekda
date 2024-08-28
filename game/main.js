da71302c0f14c66e41be2bdd93a954afa3fa9d18
Alchemist. <121161360+Rajiv2809@users.noreply.github.com>
2024-08-29 05:30:31 +0700

    ketiga

--- game/main.js
+++ game/main.js
@@ -1,8 +1,14 @@
 class Game {
     constructor(canvas){
-        this.game = ''
-        this.ball = ''
-        this.level = ''
+        this.canvas = canvas
+        this.ctx = canvas.getContext('2d');
+        this.width = canvas.width;
+        this.height = canvas.height;
+        this.counrty = null
+        this.ball = null
+        this.level = null
+        this.startGame = false;
+        this.difficulty = level;
     }
     render(){
         this.player.draw();
@@ -11,13 +17,21 @@ class Game {
 class Menu {
     constructor(game){
         this.game = game;
-        this.instructionDiv = document.querySelector('.welcome');
+        this.welcome = document.querySelector('.welcome');
         this.setUpGame = document.querySelector('.character-setup');
 
 
 
         this.agreeButton = document.getElementById('agree');
+        this.countrySelect = document.getElementById('country');
+        this.levelSelect = document.getElementById('level');
+        this.ballSelect = document.getElementById('ball');
         this.usernameInput = document.getElementById('username');
+        this.playButton = document.getElementById('play');
+
+
+        this.countdownDiv = document.querySelector('.countdown');
+        this.countdownTimer = document.getElementById('countdown-timer');
     }
     setupEventListeners() {
         this.usernameInput.addEventListener('input', () => {
@@ -28,12 +42,49 @@ class Menu {
             this.username = this.usernameInput.value.trim();
             this.showGameSetup();
         });
-        
+
+        this.levelSelect.addEventListener('change', () => {
+            this.game.level = this.levelSelect.value;
+            this.checkPlayButton();
+        });
+        this.levelSelect.addEventListener('change', () => {
+            this.game.country = this.levelSelect.value;
+            this.checkPlayButton();
+        });
+        this.ballSelect.addEventListener('change', () => {
+            this.game.ball = this.levelSelect.value;
+            console.log( this.game.ball);
+            
+            this.checkPlayButton();
+        });
+
     }
+    checkPlayButton() {
+        
+        this.playButton.disabled = !(this.game.counrty && this.game.level && this.game.ball);
+    }
+    
     showGameSetup() {
-        this.instructionDiv.classList.add('hidden');
+        this.welcome.classList.add('hidden');
         this.setUpGame.classList.remove('hidden');
     }
+    startCountdown() {
+        this.characterSelectDiv.classList.add('hidden');
+        this.countdownDiv.classList.remove('hidden');
+        let count = 3;
+        this.countdownTimer.textContent = count;
+
+        const interval = setInterval(() => {
+            count--;
+            if (count > 0) {
+                this.countdownTimer.textContent = count;
+            } else {
+                clearInterval(interval);
+                this.startGame();
+            }
+        }, 1000);
+    }
+    
 }
 
 
