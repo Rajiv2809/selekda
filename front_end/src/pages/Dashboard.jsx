e977bc59f3c1c0e1b2314d58e95fd8b04cd0a8a7
Alchemist. <121161360+Rajiv2809@users.noreply.github.com>
2024-08-30 19:31:45 +0700

    ke 29

--- front_end/src/pages/Dashboard.jsx
+++ front_end/src/pages/Dashboard.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import { useStateContext } from '../contexts/Context'
 import { Navigate } from 'react-router-dom';
+import SideBar from '../components/SideBar';
+import Nav from '../components/Nav';
 
 export default function Dashboard() {
-  const {currentuser} = useStateContext();
-  console.log(currentuser);
-  
-    // if(currentuser.role == 'user'){
-    //   <Navigate to='/home'/>
-    // }
+  const { currentUser } = useStateContext();
+  console.log(currentUser);
+
+  if (currentUser.role == 'user') {
+    <Navigate to='/home' />
+  }
   return (
-  <div>Dashboard</div>
+    <>
+      <div className='dashboard-content'>
+        <SideBar />
+        <div className='dashboard-item'>
+          <div className="nav-sidebar">
+            <div></div>
+            <div className="search-dashboard"></div>
+            <div className="profile">
+              <img src={} alt="" />
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </>
   )
 }
